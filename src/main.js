feca9522c656d7a1a0b1141d31a8d5197d2013a9
KurokuTetsuya <perumnas.good@gmail.com>
2019-07-25 20:57:03 +0700

    -_-

--- src/main.js
+++
@@ -1,6 +0,0 @@
-const { Japanisme } = require('./handler/client');
-const client = new Japanisme({
-    disableEveryone: true
-});
-
-client.login(process.env.DEV ? process.env.DEV_TOKEN : process.env.TOKEN);
