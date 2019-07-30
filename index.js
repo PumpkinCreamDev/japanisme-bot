e8b32ac9f31b976e8c1d14ac98b06b458c5ba40c
PumpkinCreamDev <52337216+PumpkinCreamDev@users.noreply.github.com>
2019-07-30 11:21:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const LoggingFactory = require("./src/handler/loggingfactory");
 const Logger = new LoggingFactory("MainProcess");
 
 if (process.argv[2] === "dev") {
-  Logger.info("Bot ini sedang dalam dev mode !");
+  Logger.info("Bot ini sedang dalam dev mode!");
   process.env.DEV = "dev";
   require("./src/shards");
 } else {
-  Logger.info("Bot ini sedang dalam normal mode !");
+  Logger.info("Bot ini sedang dalam normal mode!");
   require("./src/shards");
 }
